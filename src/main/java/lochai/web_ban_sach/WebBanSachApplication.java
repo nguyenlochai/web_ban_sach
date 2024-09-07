dd5818a5f7be5a989d66ee4fa16f24371ba665bf
nguyenlochai <hailoc198@gmail.com>
2024-09-07 15:03:57 +0700

    tao du an moi

---
+++ src/main/java/lochai/web_ban_sach/WebBanSachApplication.java
@@ -0,0 +1,13 @@
+package lochai.web_ban_sach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lochai/web_ban_sach/WebBanSachApplicationTests.java
@@ -0,0 +1,13 @@
+package lochai.web_ban_sach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
